7b9a6a76f42d482577b636a8546cb975193e9669
Tauqeer Ahmed Shakir <tauqeer.shakir@yahoo.com>
2015-04-09 07:40:14 -0700

    Changes

---
+++ CharacterAnimationMecannim/Assets/Scripts/demo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class demo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnAnimatorMove()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z += animator.GetFloat("demoAnimator") * Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }
+}
